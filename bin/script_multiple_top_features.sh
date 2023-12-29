27ecd2a2928de9216162801457e73c209a782772
davidechicco <davide.chicco@gmail.com>
2023-12-29 15:02:00 +0100

    script loop

--- bin/script_multiple_top_features.sh
+++ bin/script_multiple_top_features.sh
@@ -15,12 +15,13 @@ random_num=$((1 + $RANDOM % 10000))
 grep_lim=1
 
 start=2
-end=10
+end=5
+numIterations=100
 for((i=start;i<=end;++i));
 do
     topFeatNum=$i
-    fileName="../results/output_"$random_num"_top"$topFeatNum"features.txt";
-    Rscript random_forest_feature_selection_and_classification_afterwards.r $topFeatNum > $fileName 2> $fileName; tail $fileName
+    fileName="../results/output_"$random_num"_top"$topFeatNum"features_"$numIterations"iter.txt";
+    Rscript random_forest_feature_selection_and_classification_afterwards.r $topFeatNum $numIterations > $fileName 2> $fileName; tail $fileName
 
 #     echo -e "\n"
 #     tail -20 "../results/output_5655_top"$i"features.txt" | grep -$grep_lim "MCC"
